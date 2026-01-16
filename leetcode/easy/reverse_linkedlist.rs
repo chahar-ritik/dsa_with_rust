59f910aec018820318aadafbcb5860df292d3f73
ritik chahar <ritikchahar156@gmail.com>
2026-01-16 22:02:33 +0530

    feat: add new solution

---
+++ leetcode/easy/reverse_linkedlist.rs
@@ -0,0 +1,33 @@
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(
+        mut head: Option<Box<ListNode>>
+    ) -> Option<Box<ListNode>> {
+
+        let mut prev: Option<Box<ListNode>> = None;
+
+        while let Some(mut node) = head {
+            let next = node.next.take();
+            node.next = prev;
+            prev = Some(node);
+            head = next;
+        }
+
+        prev
+    }
+}
