da14ef871c4bb122ab2cd01c9956af79cb223567
ritik chahar <ritikchahar156@gmail.com>
2026-03-07 16:17:30 +0530

    fix: little changes in binary tree and add level order traversal

--- data_structures/tree/binary_tree.rs
+++ data_structures/tree/binary_tree.rs
@@ -1,6 +1,7 @@
 use std::cell::RefCell;
 use std::rc::Rc;
-
+use std::collections::VecDeque;
+// TreeNode that use Box and Node that use Rc and RefCell
 struct TreeNode {
     val: i32,
     left: Option<Box<TreeNode>>,
@@ -33,13 +34,17 @@ fn main() {
     postorder(&root);
 
     //Using Rc and RefCell
-    let rootnode = Rc::new(RefCell::new(Node::new(4)));
+    /*let rootnode = Rc::new(RefCell::new(Node::new(4)));
     let child_left = Rc::new(RefCell::new(Node::new(5)));
     let child_right = Rc::new(RefCell::new(Node::new(5)));
 
     rootnode.borrow_mut().left = Some(Rc::clone(&child_left));
     rootnode.borrow_mut().right = Some(Rc::clone(&child_right));
     let rootnode = Some(rootnode);
+*/
+    let preorder_seq = vec![1,2,-1,-1,3,4,-1,-1,5,-1,-1];
+    let mut index = 0;
+    let rootnode = build_tree(&preorder_seq , &mut index);
 
     println!("\nUsing Rc and RefCell\n Inorder traversal");
     inorder_rc(&rootnode);
@@ -47,6 +52,23 @@ fn main() {
     preorder_rc(&rootnode);
     println!("\nPostorder traversal");
     postorder_rc(&rootnode);
+     println!("\nLevel order traversal");
+     level_order_traversal(&rootnode);
+}
+
+fn build_tree(preorder_seq:&Vec<i32> , index: &mut usize) -> Option<Rc<RefCell<Node>>>{
+    if preorder_seq[*index] == -1 {
+          *index += 1;
+        return None;
+    }
+   let node = Rc::new(RefCell::new(Node::new(preorder_seq[*index])));
+   *index+=1;
+   {
+   let mut node_borrow = node.borrow_mut();
+   node_borrow.left = build_tree(preorder_seq,index);
+   node_borrow.right = build_tree(preorder_seq,index);
+   } // you can drop manually if don't want block as it is doing same drop(node_borrow);
+   Some(node)
 }
 
 fn inorder(node: &Option<Box<TreeNode>>) {
@@ -113,5 +135,24 @@ fn postorder_rc(node: &Option<Rc<RefCell<Node>>>) {
         postorder_rc(&rnode.borrow().left);
         postorder_rc(&rnode.borrow().right);
         print!("{} ", rnode.borrow().val);
+    }
+}
+
+fn level_order_traversal(node: &Option<Rc<RefCell<Node>>>){
+    let mut queue = VecDeque::new();
+   
+   
+    queue.push_back(node.clone());
+   
+    while let Some(root) = queue.pop_front(){{
+       
+       if let Some(node) = root{
+               
+               
+               print!("{} ", node.borrow().val);
+               queue.push_back(node.borrow().left.clone());
+               queue.push_back(node.borrow().right.clone());
+           }
+        }
     }
 }
