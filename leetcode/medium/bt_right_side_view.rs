b139b1b2cbd2f24918c33427983c77dbf4da08d7
ritik chahar <ritikchahar156@gmail.com>
2026-03-08 21:45:44 +0530

    feat: add new solution

---
+++ leetcode/medium/bt_right_side_view.rs
@@ -0,0 +1,34 @@
+pub fn right_side_view(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+        let mut queue = VecDeque::new();
+        let mut result = Vec::new();
+        if root.is_none(){
+           return result;
+        }
+          queue.push_back(root.clone());
+          queue.push_back(None);
+
+          while let Some(rnode) = queue.pop_front(){
+             if let Some(node) = rnode{
+                let node = node.borrow();
+                if let Some(val) = queue.front(){
+                   if val.is_none(){
+                      result.push(node.val);
+                   }
+                  }
+
+                if node.left.is_some(){
+                queue.push_back(node.left.clone());
+                }
+                  if node.right.is_some(){
+                queue.push_back(node.right.clone());
+                  }
+
+                  
+             }else{
+                  if !queue.is_empty(){
+                    queue.push_back(None);
+                  }
+             }
+          }
+          result
+    }
